29f855c57569f62923d99c1f33ff55792e2a8b4c
Nathan Hughes <nathan1hughes@gmail.com>
2019-01-24 14:36:53 +0000

    Updated to use yield

--- pd_diffuse.py
+++ pd_diffuse.py
@@ -7,46 +7,74 @@ sns.set()
 
 
 def diffuse_vectorise(un, g, b, dt, dx2, a):
-    return (un[1:-1] + a * dt/dx2 * (un[0:-2] - 2 * un[1:-1] + un[2:])) + b
+    """
+    Takes a state, rate of decay, production, delta time, delta space and
+    flux of molecule. Uses these data to compute next time state
+
+    """
+    return (un[1:-1] + a * dt/dx2 * (un[0:-2] - 2 * un[1:-1] + un[2:])) * g + b
 
 
 def diffuse_point(c1, c2, c3, a, g, b):
+    """
+    Takes 3 concentration points and diffuses between them
+    uses a diffusiveness of PD to estimate passage through
+    """
+    def Q(A):
+        return A * 1
     return Q(a*(c1 - 2*c2 + c3)) * g + b
 
 
-def Q(A):
-    return A * 1
-
-
-def bs():
-    return [cells.shape[0]//2]
+def bs(c, th):
+    """
+    Provides a method for highlighting cells which
+    qualify for a beta production
+    """
+    b = []
+    for i in range(c.shape[0]):
+        if c[i].sum() > th:
+            b.append(i)
+    return b
 
 
 def stokes_einstein(x): return ((1.38e-23 * 298.15)/(6*np.pi * 8.9e-4 * x))
 
 
-def C(dx, nt, a, dt, g, b, c, num_iter, bs):
+def C(dx, nt, a, dt, g, b, c, num_iter, bs, th, astimeseries=False):
+    """
+    takes a delta x, number of timepoints to compute, alpha diffusiveness of
+    molecule delta time, gamma decay, beta production, current concentration
+    matrix, number of iterations completed already and a function to derive
+    cells which will produce the beta term
+    """
     dx2 = dx**2
     start = num_iter
     cells = c.copy()
     for n in range(start, nt):
+        production = bs(cells, th)
         for idx in range(cells.shape[0]):
+            bb = 0
+            if idx in production:
+                bb = b
             u = cells[idx].copy()
             un = u.copy()
-            u[1:-1] = diffuse_vectorise(un, g, b, dt, dx2, a)
+            u[1:-1] = diffuse_vectorise(un, g, bb, dt, dx2, a)
 
             if idx < cells.shape[0]-1:
                 u[-1] = diffuse_point(cells[idx+1][0],
                                       u[-1],
-                                      u[-2], a, g, b)
+                                      u[-2], a, g, bb)
 
             if idx > 0:
                 u[0] = diffuse_point(u[1],
                                      u[0],
-                                     cells[idx-1][-1], a, g, b)
+                                     cells[idx-1][-1], a, g, bb)
 
             cells[idx] = u
-    return cells
+        if astimeseries:
+            print('ts')
+            yield cells.copy()
+    yield cells
 
 
 N = 11  # -5 +5
@@ -60,32 +88,36 @@ g = 1
 b = 0
 cells = np.zeros((N, Xs))
 cells[cells.shape[0]//2] = 1
-
+th = 1
 
 a = stokes_einstein(3.5e-10) * 1e+6  # mm per second ^2
 
-cells = C(dx, t, a, dt, g, b, cells, 0)
-
-
-fig, axes = plt.subplots(2, sharex=True)
+cells = C(dx, t, a, dt, g, b, cells, 0, bs, th)
 
 
 # Plotting below here
 
-x_labels_locations = np.linspace(0, N*Xs, num=11)
-x_labels_locations = [50+(i*100) for i in range(11)]
 
-x_labels = ['C{0}'.format(n) for n in range(11)]
+def plot_final_state(cells, N, Xs):
+    fig, axes = plt.subplots(2, sharex=True)
+    x_labels_locations = np.linspace(0, N*Xs, num=11)
+    x_labels_locations = [50+(i*100) for i in range(11)]
+
+    x_labels = ['C{0}'.format(n) for n in range(11)]
+
+    axes[0].plot(cells.ravel())
+    axes[0].set_yscale('log')
+
+    pcm = axes[1].pcolormesh(np.expand_dims(cells.ravel(),
+                                            axis=0),
+                             norm=LogNorm(vmin=cells[cells > 0].min(),
+                                          vmax=cells.max()))
+
+    plt.sca(axes[1])
+    plt.xticks(x_labels_locations, x_labels)
+    plt.colorbar(pcm, orientation="horizontal", pad=0.2)
+    plt.tight_layout()
+    plt.show()
 
 
-axes[0].plot(cells.ravel())
-axes[0].set_yscale('log')
-
-pcm = axes[1].pcolormesh(np.expand_dims(cells.ravel(), axis=0), norm=LogNorm(
-    vmin=cells[cells > 0].min(), vmax=cells.max()))
-
-plt.sca(axes[1])
-plt.xticks(x_labels_locations, x_labels)
-plt.colorbar(pcm, orientation="horizontal", pad=0.2)
-plt.tight_layout()
-plt.show()
+plot_final_state(next(cells), N, Xs)
